2a3bdb1af721876e3d126b3e8513ec489fd0abcc
Marco Meyer-Conde <marco.meyerconde@gmail.com>
2024-12-05 06:50:06 +0000

    Initial commit

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,5 @@
 #! /usr/bin/bash
 
-./configure --prefix=$PREFIX
-
-make -j$(nproc)
-make install
+OVERRIDES="PROF_VERSION=${PKG_VERSION} CPPFLAGS=-I${PREFIX}/include/eigen3 PREFIX=${PREFIX}"
+make $OVERRIDES
+make install $OVERRIDES
